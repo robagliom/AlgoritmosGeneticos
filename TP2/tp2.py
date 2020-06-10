773c41ac38d7cefd5a1a16f46c73e1bd013a109c
Magdalena Robaglio <robaglio.magdalena@gmail.com>
2020-06-09 22:19:09 -0300

    ejercicio 3

--- TP2/tp2.py
+++ TP2/tp2.py
@@ -5,20 +5,11 @@ from pick import pick
 #Librería que genera combinaciones
 import itertools
 
-####################
-#Variables globales#
-####################
-#Volumen máximo mochila en cm3
-Vmax = 4200
-#Lista de objetos posibles
-#Tupla: (volumen objeto i en cm3, valor objeto i en $)
-objetos = [[150,20],[325,40],[600,50],[805,36],[430,25],[1200,64],[770,54],[60,18],[930,46],[353,28]]
-
 #####################
 #Búsqueda exhaustiva#
 #####################
 #Queremos Máximizar Valor $
-def maximo_valor_posible(lista_objetos):
+def maximo_valor_posible(lista_objetos,Vmax):
     valor_acum = 0
     vol_acum = 0
     for objeto in lista_objetos:
@@ -38,14 +29,14 @@ def generar_combinaciones(lista_objetos):
         lista_combinaciones.append(itertools.combinations(lista_objetos,i))
     return lista_combinaciones
 
-def busqueda_exhaustiva():
+def busqueda_exhaustiva(Vmax,objetos):
     valor_max = 0
     vol_acum_max = 0
     lista_solucion_max = []
     combinaciones_posibles = generar_combinaciones(objetos)
     for combinacion_i in combinaciones_posibles:
         for lista_i in combinacion_i:
-            valor_max_i,vol_acum_i = maximo_valor_posible(lista_i)
+            valor_max_i,vol_acum_i = maximo_valor_posible(lista_i,Vmax)
             if valor_max_i > valor_max:
                 valor_max = valor_max_i
                 vol_acum_max = vol_acum_i
@@ -58,11 +49,11 @@ def busqueda_exhaustiva():
 def proporcion_objeto(objeto):
     return objeto[1]/objeto[0]
 
-def algoritmo_greedy():
+def algoritmo_greedy(Vmax,lista_objetos):
     valor = 0
     vol_acum = 0
     lista_solucion = []
-    lista_objetos = objetos
+    #lista_objetos = objetos
     for obj in lista_objetos:
         prop_obj = proporcion_objeto(obj)
         #agrego las proporciones a cada objeto
@@ -82,31 +73,52 @@ def algoritmo_greedy():
 ####################
 #Programa Principal#
 ####################
-def programa_principal(busqueda):
+def programa_principal(Vmax,objetos,busqueda):
     if busqueda=="exhaustiva":
-        valor_max,vol_acum_max,lista_solucion_max=busqueda_exhaustiva()
+        valor_max,vol_acum_max,lista_solucion_max=busqueda_exhaustiva(Vmax,objetos)
     elif busqueda=="greedy":
-        valor_max,vol_acum_max,lista_solucion_max=algoritmo_greedy()
+        valor_max,vol_acum_max,lista_solucion_max=algoritmo_greedy(Vmax,objetos)
     else:
         return
-    print("Valor máximo $",valor_max)
-    print("Volumen máximo cm3",vol_acum_max)
-    print("Objetos elegidos",lista_solucion_max)
 
-    return valor_max,vol_acum_max,lista_solucion_max
+    return "Valor máximo: ${}.\nVolumen máximo: {}cm3.\nObjetos elegidos: {}".format(valor_max,vol_acum_max,lista_solucion_max)
 
 ######
 #Menú#
 ######
 def menu():
-    que_hacer = 'Problema de la Mochila'
-    opciones_que_hacer = ['1) Búsqueda Exhaustiva','2) Algoritmo Greedy', 'X SALIR']
-    opcion_que_hacer, index = pick(opciones_que_hacer, que_hacer)
-    if opcion_que_hacer == '1) Búsqueda Exhaustiva':
-        programa_principal("exhaustiva")
-    elif opcion_que_hacer == '2) Algoritmo Greedy':
-        programa_principal("greedy")
+    que_problema = 'Problema de la Mochila'
+    opciones_mochila = ['10 objetos','3 objetos','X SALIR']
+    opcion_mochila, index_mochila = pick(opciones_mochila, que_problema)
+    if opcion_mochila == '10 objetos':
+        #Volumen máximo mochila en cm3
+        Vmax = 4200
+        #Lista de objetos posibles
+        #Tupla: (volumen objeto i en cm3, valor objeto i en $)
+        objetos = [[150,20],[325,40],[600,50],[805,36],[430,25],[1200,64],[770,54],[60,18],[930,46],[353,28]]
+    elif opcion_mochila == '3 objetos':
+        #Volumen máximo mochila en cm3
+        Vmax = 3000
+        #Lista de objetos posibles
+        #Tupla: (volumen objeto i en cm3, valor objeto i en $)
+        objetos = [[1800,72],[600,36],[1200,60]]
     else:
         sys.exit()
+    que_metodo = 'Con qué método va a resolver el problema de la mochila?'
+    opciones_metodo = ['1) Búsqueda Exhaustiva','2) Algoritmo Greedy', 'X VOLVER']
+    opcion_metodo, index = pick(opciones_metodo, que_metodo)
+    if opcion_metodo == 'X VOLVER':
+        menu()
+    else:
+        if opcion_metodo == '1) Búsqueda Exhaustiva':
+            busqueda = 'exhaustiva'
+        elif opcion_metodo == '2) Algoritmo Greedy':
+            busqueda = 'greedy'
+        resultado=programa_principal(Vmax,objetos,busqueda)
+        seguir, index = pick(['SI','X SALIR'], '{}.\n\nContinuar?'.format(resultado))
+        if seguir == 'SI':
+            menu()
+        else:
+            sys.exit()
 
 menu()
